935792317ecfa37e711670b7b81573add2b9c448
Danilo Correa <danilosilva87@gmail.com>
2023-02-20 10:56:52 -0300

    chore(terraform): alterado dados de conexao do terraform

--- iac/main.tf
+++ iac/main.tf
@@ -9,12 +9,12 @@ terraform {
     required_version = ">= 1.0.0"
 
     backend "s3" {
-        bucket = "autodoc-terraform"
-        key = "example-net6/terraform.tfstate"
+        bucket = "dcorrea-terraform"
+        key = "example-net7/terraform.tfstate"
         region = "us-east-1"
         shared_credentials_file = "~/.aws/credentials"
-        profile = "terraform-homolog"
-    }    
+        profile = "danilosilva87"
+    }
 }
 
 provider "aws" {
